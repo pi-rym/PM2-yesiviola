154ce7ed4ac3cf81b107b2a9146d560acd049bb4
yesenia <aryesigon@gmail.com>
2024-03-19 01:09:46 -0300

    homework dia lunes

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -2,7 +2,7 @@
  const renderCards = require('./renderCards');
 document.addEventListener('DOMContentLoaded', async (event) => {
   try {
- const response = await axios.get("https://localhost:3000/movies");
+ const response = await axios.get("http://localhost:3000/movies");
     renderCards(response.data);
   } catch (error) {
     console.error(error);
